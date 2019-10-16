25513121b14be6971e50d6d0a399229df11bf9f1
Juan de Bruin <juandb@pm.me>
2019-10-16 21:53:52 +0200

    Fix compile errors

--- hmac.go
+++ hmac.go
@@ -12,22 +12,22 @@ func HMAC(key, message []byte, hash hash.Hash) (mac []byte) {
 
 	if len(key) < hash.BlockSize() {
 		padding := make([]byte, hash.BlockSize()-len(key))
-		key = append(key, padding)
+		key = append(key, padding...)
 	}
 
 	ikp := make([]byte, len(key))
-	for i, v := range key {
+	for i := range key {
 		ikp[i] = key[i] ^ 0x36
 	}
-	hash.Write(append(ikp, message))
+	hash.Write(append(ikp, message...))
 	message = hash.Sum([]byte{})
 	hash.Reset()
 
 	okp := make([]byte, len(key))
-	for i, v := range key {
+	for i := range key {
 		okp[i] = key[i] ^ 0x5c
 	}
-	hash.Write(append(okp, message))
+	hash.Write(append(okp, message...))
 	mac = hash.Sum([]byte{})
 	hash.Reset()
 
