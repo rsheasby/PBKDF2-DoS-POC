c8381ee9f3e57e2a129ea8dc5546c979168edd7f
Ryan David Sheasby <ryan@sheasby.dev>
2019-10-17 17:04:01 +0200

    Improve comments

--- pbkdf2.go
+++ pbkdf2.go
@@ -12,15 +12,15 @@ func Pbkdf2HmacSha256(password, salt []byte, iterations, outputLen int) (derived
 	// Allocate output array
 	derivedKey = make([]byte, blocks*32)
 	for b := 0; b < blocks; b++ {
+		// Do all the hashing
 		var bBytes [4]byte
 		binary.BigEndian.PutUint32(bBytes[:], uint32(b+1))
 		state := make([][]byte, iterations)
-		// Do all the hashing
 		state[0] = HmacSha256(password, append(salt, bBytes[:]...))
 		for i := 1; i < iterations; i++ {
 			state[i] = HmacSha256(password, state[i-1])
 		}
-		// Xor all the results together
+		// XOR all the results together
 		copy(derivedKey[32*b:32*b+32], state[0][:])
 		for i := 1; i < len(state); i++ {
 			for bi := range state[i] {
