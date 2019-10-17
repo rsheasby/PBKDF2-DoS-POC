8a99b6b04ff89178653de751b58e35980b275111
Ryan David Sheasby <ryan@sheasby.dev>
2019-10-17 11:58:28 +0200

    Separate length normalization into separate function

--- hmac.go
+++ hmac.go
@@ -2,9 +2,7 @@ package main
 
 import "crypto/sha256"
 
-// HMAC This function assumed the input hash is in a clean state.
-func HMAC(key, message []byte) (mac []byte) {
-	// Normalize key length
+func HmacSha256NormalizeKey(key []byte) []byte {
 	if len(key) > sha256.BlockSize {
 		tmp := sha256.Sum256(key)
 		key = tmp[:]
@@ -13,6 +11,14 @@ func HMAC(key, message []byte) (mac []byte) {
 		paddingLen := sha256.BlockSize - len(key)
 		padding := make([]byte, paddingLen)
 		key = append(key, padding...)
+	}
+	return key
+}
+
+func HmacSha256(key, message []byte) (mac [32]byte) {
+	// Normalize key length if necessary
+	if len(key) != sha256.BlockSize {
+		key = HmacSha256NormalizeKey(key)
 	}
 
 	// Generate ipad and opad
@@ -27,5 +33,5 @@ func HMAC(key, message []byte) (mac []byte) {
 	ihash := sha256.Sum256(append(ipad, message...))
 	ohash := sha256.Sum256(append(opad, ihash[:]...))
 
-	return ohash[:]
+	return ohash
 }
--- hmac_test.go
+++ hmac_test.go
@@ -20,12 +20,12 @@ func equal(a, b []byte) bool {
 	return true
 }
 
-func TestHMAC_SHA256(t *testing.T) {
+func TestHmacSha256(t *testing.T) {
 	expected, _ := hex.DecodeString("f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8")
-	actual := HMAC([]byte("key"), []byte("The quick brown fox jumps over the lazy dog"))
+	actual := HmacSha256([]byte("key"), []byte("The quick brown fox jumps over the lazy dog"))
 	fmt.Println(expected)
 	fmt.Println(actual)
-	if !equal(expected, actual) {
+	if !equal(expected, actual[:]) {
 		t.Errorf("HMAC wrong!\n")
 	}
 }
